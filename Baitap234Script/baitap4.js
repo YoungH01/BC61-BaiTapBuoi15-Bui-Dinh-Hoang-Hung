cf300b0228114ac6a64c98992475f276d6a185e5
Bùi Đình Hoàng Hùng <77423945+YoungH01@users.noreply.github.com>
2024-01-13 00:20:32 +0700

    Update baitap4.js

--- Baitap234Script/baitap4.js
+++ Baitap234Script/baitap4.js
@@ -31,7 +31,7 @@ function calculateAns(){
         }else ans +=(10*75 +(Number(input4.value)-10)*5);
     }
     ans=ans.toFixed(2);
-    ans=new Intl.NumberFormat('vn-VN').format(ans);
+    //ans=new Intl.NumberFormat('vn-VN').format(ans);
     text_ans="Mã khách hàng: "+input2.value +"; Tiền cáp: $"+ ans.toString();
     result.innerHTML=text_ans;
     return ;
